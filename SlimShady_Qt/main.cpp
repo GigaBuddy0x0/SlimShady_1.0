82aefcd9a086238693ad8ff142de682bfe11fbc9
Alex <Alexsmirniy11@gmail.com>
2023-11-19 20:46:35 +0300

    The basis are ready

--- SlimShady_Qt/main.cpp
+++ SlimShady_Qt/main.cpp
@@ -4,7 +4,36 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    cout << "Input the two numbers" << endl;
+    double a, b;
+    cin >> a;
+    cin >> b;
+    cout << "What did you wanna do with your numbers?" << endl;
+    cout << "Input: '1' if you wanna get summ" << endl
+         << "       '2' if you wanna get difference." << endl
+         << "       '3' if you wanna get multiplicate" << endl
+         << "       '4' if you wanna get division" << endl;
+    char n;
+    cin >> n;
+    cout << "And your answer is: ";
+    switch (n)
+    {
+        case '1':
+            cout << "The feature is still development. Sorry" << endl;
+            break;
+        case '2':
+            cout << "The feature is still development. Sorry" << endl;
+            break;
+        case '3':
+            cout << "The feature is still development. Sorry" << endl;
+            break;
+        case '4':
+            cout << "The feature is still development. Sorry" << endl;
+            break;
+        default:
+            cout << "Not like that! Goodby." <<endl;
+            break;
+    }
+    system("pause");
     return 0;
 }
-
